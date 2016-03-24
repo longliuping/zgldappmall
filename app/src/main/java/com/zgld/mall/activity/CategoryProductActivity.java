e11d3c89882c5ea392ccf37303a9fd82d45131b7
longliuping <z819366568>
2016-03-24 10:48:05 +0800

    spinner

--- app/src/main/java/com/zgld/mall/activity/CategoryProductActivity.java
+++ app/src/main/java/com/zgld/mall/activity/CategoryProductActivity.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 import com.zgld.mall.R;
 import com.zgld.mall.beans.HotCategory;
 import com.zgld.mall.utils.Contents;
+import com.zgld.mall.widget.NiceSpinner;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CategoryProductActivity extends BaseActivity {
     HotCategory info = null;
@@ -31,6 +36,10 @@ public class CategoryProductActivity extends BaseActivity {
         }
         TextView title = (TextView) findViewById(R.id.title_center);
         title.setText(info.getHotname());
+
+        NiceSpinner niceSpinner = (NiceSpinner) findViewById(R.id.nice_spinner_province);
+        List<String> dataset = new LinkedList<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
+        niceSpinner.attachDataSource(dataset);
     }
 
     @Override
--- app/src/main/java/com/zgld/mall/activity/LoadActivity.java
+++ app/src/main/java/com/zgld/mall/activity/LoadActivity.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 import android.os.Message;
 
 import com.zgld.mall.R;
+import com.zgld.mall.beans.Region;
+import com.zgld.mall.utils.XMLAddressHandler;
+import com.zgld.mall.utils.XMLContentHandler;
+
+import java.io.InputStream;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class LoadActivity extends BaseActivity {
 
@@ -14,6 +23,19 @@ public class LoadActivity extends BaseActivity {
         setContentView(R.layout.activity_load);
         startActivity(new Intent(this, MainActivity.class));
         finish();
+        try {
+            InputStream inStream = this.getResources().getAssets().open("address.xml");
+            SAXParserFactory spf = SAXParserFactory.newInstance();// 创建解析器
+            SAXParser saxParser = spf.newSAXParser();
+            // 设置解析器的相关特性，true表示开启命名空间特性
+            // saxParser.setProperty("http://xml.org/sax/features/namespaces",
+            // true);
+            XMLAddressHandler handler = new XMLAddressHandler();
+            saxParser.parse(inStream, handler);
+            inStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
