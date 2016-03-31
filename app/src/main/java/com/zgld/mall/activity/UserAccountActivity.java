ddec673086ffaaeaa4caa49603ba646a1769b88c
longliuping <z819366568>
2016-03-31 10:47:18 +0800

    spinner

---
+++ app/src/main/java/com/zgld/mall/activity/UserAccountActivity.java
@@ -0,0 +1,32 @@
+package com.zgld.mall.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zgld.mall.R;
+
+public class UserAccountActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initStyle();
+        setContentView(R.layout.activity_user_account);
+        TextView title = (TextView) findViewById(R.id.title_center);
+        title.setText("我的账户");
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void handleMsg(Message msg) {
+
+    }
+}
